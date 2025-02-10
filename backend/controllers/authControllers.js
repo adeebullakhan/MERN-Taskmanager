5ef407b029d43f9f5ef71e5db137e44a709842a2
Adeeb Khan <adeebkhan795@gmail.com>
2025-02-10 22:11:46 +0530

    added the error data

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -59,6 +59,7 @@ exports.login = async (req, res) => {
   }
   catch (err) {
     console.error(err);
+    console.error(err.body)
     return res.status(500).json({ status: false, msg: "Internal Server Error" });
   }
 }
