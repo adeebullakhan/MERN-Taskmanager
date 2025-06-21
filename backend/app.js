41075ecc92c507876aa173248e2e82ef218b7f05
Adeeb Khan <adeebkhan795@gmail.com>
2025-06-22 03:57:29 +0530

    fixed cors in backend

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
+     console.log("Request origin:", origin); 
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
